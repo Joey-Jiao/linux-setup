a72e3aa43e13c09ed06af97d92b0febcbcc0839f
Jay W <977482173@qq.com>
2024-03-02 21:33:13 +0800

    modify

--- ubuntu-22.04.sh
+++ ubuntu-22.04.sh
@@ -49,10 +49,12 @@ printf "${GREEN}<<< 3. cmd environment installed <<<${BLANK}\n";
 # conda env
 printf "${GREEN}>>> 4. installing conda env >>>${BLANK}\n"
 curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
+mkdir ${HOME}/conda
 zsh Miniforge3-$(uname)-$(uname -m).sh -b -p "${HOME}/conda"
+sleep 2
 source "${HOME}/conda/etc/profile.d/conda.sh"
 conda activate
-conda -v
+conda --version
 printf "${GREEN}<<< 4. conda env installed <<<${BLANK}\n"
 
 #sudo reboot
