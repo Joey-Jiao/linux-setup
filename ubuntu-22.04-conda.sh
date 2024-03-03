aecb4c1c7ee7bb0728b39fee819a48c26adf9fc1
Jay W <977482173@qq.com>
2024-03-03 22:05:45 +0800

    modify

--- ubuntu-22.04-conda.sh
+++ ubuntu-22.04-conda.sh
@@ -3,8 +3,10 @@
 # conda env
 curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
 mkdir ${HOME}/conda
+su - $(whoami)
 zsh Miniforge3-$(uname)-$(uname -m).sh -b -p "${HOME}/conda"
 sleep 2
+
 source "${HOME}/conda/etc/profile.d/conda.sh"
 conda activate
 conda --version
