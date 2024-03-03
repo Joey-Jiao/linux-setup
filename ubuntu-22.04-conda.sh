7503559e2a84b928c9891707aab47c054d18eb31
Jay W <977482173@qq.com>
2024-03-03 22:12:22 +0800

    modify

--- ubuntu-22.04-conda.sh
+++ ubuntu-22.04-conda.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 # conda env
-curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
+sudo curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
 mkdir /home/$(whoami)/conda
 zsh Miniforge3-$(uname)-$(uname -m).sh -b -p "/home/$(whoami)/conda"
 sleep 2
