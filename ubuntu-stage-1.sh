9484496da0f84a02099ca4f5dc528f35407dfe24
Jay W <977482173@qq.com>
2024-03-03 21:59:31 +0800

    modify

--- ubuntu-stage-1.sh
+++ ubuntu-stage-1.sh
@@ -1,6 +1,12 @@
 #!/bin/zsh
 
 
+RED='\033[0;31m';
+GREEN='\033[0;32m';
+YELLOW='\033[1;33m';
+BLANK='\033[0m';
+
+printf "${GREEN}>>> 4. customize zsh >>>${BLANK}\n"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sleep 2
 
@@ -9,5 +15,12 @@ cp ~/.zshrc ~/.zshrc.copy
 sed -i "s@robbyrussell@powerlevel10k/powerlevel10k@g" ~/.zshrc
 source ~/.zshrc
 
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone https://github.com/paulirish/git-open.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/git-open
 
+sed -i "s@plugins=(git)@plugins=(zsh-autosuggestions zsh-syntax-highlighting zsh-history-substring-search git-open git)@g" ~/.zshrc
+
+printf "${GREEN}<<< 4. customize zsh completed <<<${BLANK}\n"
 
