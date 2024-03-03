a5154d6f81183d56fb10f325c86f9d658aef92ce
Jay W <977482173@qq.com>
2024-03-03 13:51:48 +0800

    modify

--- ubuntu-22.04-n.sh
+++ ubuntu-22.04-n.sh
@@ -1,6 +1,8 @@
 #!/bin/zsh
 
 # npm env
-#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+source ~/.zshrc
+nvm -v
