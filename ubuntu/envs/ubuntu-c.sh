71733ab484c4844d06dd861d5cdbeef355b0717d
Jay W <977482173@qq.com>
2024-03-11 13:56:20 +0800

    modify

--- ubuntu/envs/ubuntu-c.sh
+++ ubuntu/envs/ubuntu-c.sh
@@ -2,7 +2,12 @@
 
 printf "${GREEN}>>> 5.1 setup c >>>${BLANK}\n"
 
-sudo apt install build-essential cmake gdb -y
+sudo apt install build-essential cmake gdb autoconf-archive automake libtool cpanminus bison -y
+sudo cpanm IPC:Cmd
+sudo apt install libgdal-dev pkg-config libpq-dev libpq5 -y
 sudo apt install gcc-multilib -y
+
+mkdir ${HOME}/
+git clone https://github.com/microsoft/vcpkg  ${HOME}
 
 printf "${GREEN}<<< 5.1 setup c <<<${BLANK}\n"
--- ubuntu/envs/ubuntu-conda.sh
+++ ubuntu/envs/ubuntu-conda.sh
@@ -5,11 +5,12 @@ printf "${GREEN}>>> 5.2 setup conda >>>${BLANK}\n"
 sudo apt install python2
 
 sudo curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
-#mkdir /home/$(whoami)/conda
-sh Miniforge3-$(uname)-$(uname -m).sh -b -p "/home/$(whoami)/conda"
+mkdir ~/repo/conda
+sh Miniforge3-$(uname)-$(uname -m).sh -b -p "~/repo/conda"
 sleep 2
 
-source "/home/$(whoami)/conda/etc/profile.d/conda.sh"
+source "~/repo/conda/etc/profile.d/conda.sh"
+conda init zsh
 conda activate
 conda --version
 
