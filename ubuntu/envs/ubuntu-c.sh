dbee0a2dca95821dcb8fedc8569d0402c9a3762f
Jay W <977482173@qq.com>
2024-03-14 00:33:33 +0800

    modify

--- ubuntu/envs/ubuntu-c.sh
+++ ubuntu/envs/ubuntu-c.sh
@@ -2,9 +2,10 @@
 
 printf "${GREEN}>>> 5.1 setup c >>>${BLANK}\n"
 
-sudo apt install build-essential cmake gdb autoconf-archive automake libtool cpanminus bison -y
+sudo apt install build-essential cmake gdb autoconf automake libtool cpanminus bison ninja-build -y
 sudo cpanm IPC:Cmd
-sudo apt install libgdal-dev pkg-config libpq-dev libpq5 -y
+sudo apt install python3-disutils gperf nasm -y
+sudo apt install libx11-dev libxft-dev libxext-dev libdbus-1-dev libxi-dev libxtst-dev libgl1-mesa-dev libgles2-mesa-dev libglu1-mesa-dev libudev-dev libx11-xcb-dev libxcursor-dev libxdamage-dev libxinerama-dev libxrandr-dev -y
 sudo apt install gcc-multilib -y
 
 mkdir ${HOME}/
