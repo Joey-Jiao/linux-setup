2729a2e2abbff34dfa88e52ee64d5b2479ca45d9
Jay W <977482173@qq.com>
2024-08-29 14:37:44 -0400

    modify

--- ubuntu/envs/ubuntu-conda.sh
+++ ubuntu/envs/ubuntu-conda.sh
@@ -5,11 +5,11 @@ printf "${GREEN}>>> 5.2 setup conda >>>${BLANK}\n"
 sudo apt install python2
 
 sudo curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
-mkdir ~/repo/conda
+mkdir "~/repo/conda"
 sh Miniforge3-$(uname)-$(uname -m).sh -b -p "~/repo/conda"
 sleep 2
 
-source ~/repo/conda/etc/profile.d/conda.sh
+source "~/repo/conda/etc/profile.d/conda.sh"
 conda init zsh
 conda activate
 conda --version
